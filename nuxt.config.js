552a53ae3ebeb903f3fc01b2eea32799f925fd7d
Anton Milyaev <milyaev.dev@gmail.com>
2019-06-08 23:14:31 +0300

    delete languages in i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,9 +64,9 @@ export default {
     ['nuxt-i18n',{
       locales: [
         {code: "zh", iso: 'zh-CN', file: "zh.js", name: '中国  '},
-        {code: "jp", iso: 'jp-JP', file: "jp.js", name: '日本語'},
-        {code: "de", iso: 'de-DE', file: "de.js", name: 'Deutsch'},
-        {code: "fr", iso: 'fr-FR', file: "fr.js", name: 'France'},
+        // {code: "jp", iso: 'jp-JP', file: "jp.js", name: '日本語'},
+        // {code: "de", iso: 'de-DE', file: "de.js", name: 'Deutsch'},
+        // {code: "fr", iso: 'fr-FR', file: "fr.js", name: 'France'},
         {code: "pt", iso: 'pt-PT', file: "pt.js", name: 'Portugal'},
         {code: "es", iso: 'es-ES', file: "es.js", name: 'Español'},
         {code: "ru", iso: 'ru-RU', file: "ru.js", name: 'Русский'},
