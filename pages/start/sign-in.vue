6346e025fba1171be875d1e6fd6d0aacb23b20fc
Anton Milyaev <milyaev.dev@gmail.com>
2019-06-10 03:09:12 +0300

    fix url redirect

--- pages/start/sign-in.vue
+++ pages/start/sign-in.vue
@@ -47,7 +47,7 @@ export default {
         });
 
         this.$router.push({
-            path: this.$route.query.redirect || "ru/my"
+            path: this.$route.query.redirect || "/ru/my"
           })
 
       } catch (e) {
