7c15843bf5cfe6e3f373c4a3f211811b0e9b3d01
Anton Milyaev <milyaev.dev@gmail.com>
2019-06-16 21:58:59 +0300

    add view workout

--- pages/my/workouts/index.vue
+++ pages/my/workouts/index.vue
@@ -8,6 +8,75 @@
       <p>
         Не нужно дожидаться прихода в зал, чтобы решить, чем вы будете заниматься сегодня, какие мышцы и каким образом тренировать. На это расходуется масса времени. Идите в зал с четким планом тренировки, из которого вы точно будете знать, что вам нужно нагружать сегодня.
       </p>
+
+      <div class="workout-cards">
+
+        <a href="#">
+          <div class="card">
+            <h2>Золотая тройка паурлифтинга</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+            <a class="btn btn-card-edit btn-color-yellow" href="#">
+              <img src="~/static/assets/img/_src/icon/gear.svg">
+            </a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Групповые занятия на рельефные ноги</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Заголовок тренировки</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Заголовок тренировки</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Заголовок тренировки</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Заголовок тренировки</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Заголовок тренировки</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="card">
+            <h2>Заголовок тренировки</h2>
+            <a class="btn btn-card-more btn-color-gradient-gray" href="#">Подробнее</a>
+          </div>
+        </a>
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -22,5 +91,64 @@ export default {
 }
 </script>
 
-<style>
+<style lang="sass">
+.workout-cards
+  width: 100%
+  display: grid
+  grid-template-columns: repeat(auto-fill, minmax(20.000em, 1fr))
+  grid-gap: 2vw
+  .card
+    background: #d1d1d1
+    border-radius: 20px
+    height: 14.2em
+    background: linear-gradient(180deg, rgba(155, 155, 155, 0) 3.36%, rgba(60, 60, 60, 0.8) 81.59%), url(https://cdn.diabetesdaily.com/wp-content/blogs.dir/21/files/2018/05/victor-freitas-546968-unsplash.jpg);
+    background-size: cover
+    opacity: 0.9
+    color: #fff
+    position: relative
+    -webkit-transition: opacity .2s linear
+    -moz-transition: opacity .2s linear
+    -o-transition: opacity .2s linear
+    transition: opacity .2s linear
+    overflow: hidden
+    &:hover
+      box-shadow: 0px 8px 25px rgba(122, 122, 122, 0.44)
+      opacity: 1
+    &:hover
+      > h2
+        bottom: 3em
+      > .btn
+        bottom: 1em
+        opacity: 1
+    h2
+      font-size: 1.2em
+      letter-spacing: 0.04em
+      line-height: 1.2em
+      position: absolute
+      bottom: 0
+      padding: 1.15em 2.3em
+      -webkit-transition: bottom .2s ease-in-out
+      -moz-transition: bottom .2s ease-in-out
+      -o-transition: bottom .2s ease-in-out
+      transition: bottom .2s ease-in-out
+    .btn
+      text-decoration: none
+      position: absolute
+      bottom: -3.125em
+      opacity: 0
+      -webkit-transition: bottom .25s, opacity .15s ease-in-out
+      -moz-transition: bottom .25s, opacity .15s ease-in-out
+      -o-transition: bottom .25s, opacity .15s ease-in-out
+      transition: bottom .25s, opacity .15s ease-in-out
+      &-card-more
+        margin: 1.15em 3em
+        padding: 1em 3.5em
+      &-card-edit
+        border-radius: 99px
+        right: 1em
+        margin: 1.15em 2em
+        padding: 0.9em 1.02em 0.5em 1.1em
+        img
+          height: 1.625em
+
 </style>
