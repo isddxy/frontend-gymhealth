08d9a54c9f586fd00a38e886b59a8f6bad9f37bc
Anton Milyaev <milyaev.dev@gmail.com>
2019-07-11 20:03:33 +0300

    merge files .scss to .sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,8 +33,8 @@ export default {
   ** Global CSS
   */
   css: [
-    "@/assets/theme/main/style/sass/main.sass",
-    "@/assets/theme/main/style/scss/app.scss"
+    "@/assets/theme/main/style/sass/main2.sass",
+    //"@/assets/theme/main/style/scss/app.scss"
   ],
 
 
